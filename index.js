e1b25b175cf1a288916e70cfcb526b24218c7c2b
MonsterMannen <viktorsvensson94@gmail.com>
2018-02-03 20:21:57 +0100

    adding files

---
+++ index.js
@@ -0,0 +1,40 @@
+const request = require('request');
+
+exports.getStats = (username, platform, callback) => {
+
+    var url = "https://fortnitetracker.com/profile/";
+    url += platform + "/" + username;
+
+    request(url, (error, response, body) => {
+        // error
+        if(!response || response.statusCode != 200){
+            var ret = { status: "player not found" };
+            callback(ret);
+            return;
+        }
+
+        // get relevant json text from html file
+        var last7 = body.substring(body.indexOf("var Last7") + 12);
+        last7 = last7.substring(0, last7.indexOf("</script>") - 1);
+
+        // parse to json object
+        var json = JSON.parse(last7);
+
+        // obtain each value and put in dict
+        var ret = {
+            status: "ok",
+            score: json[0].value,
+            kills: json[1].value,
+            wins: json[2].value,
+            matches: json[3].value,
+            top_3_5_10: json[5].value,
+            top_6_12_25: json[6].value,
+            kd: json[7].displayValue,
+            wr: json[8].value,
+            minutesPlayed: json[9].value
+        }
+
+        // callback function with result
+        callback(ret);
+    });
+}
